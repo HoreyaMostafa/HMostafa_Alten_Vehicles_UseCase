614f7f3025794d62fe000289af6b184cb36c93e1
Horeya Mostafa <39738542+HoreyaMostafa@users.noreply.github.com>
2019-09-26 00:33:39 +0200

    Continue

---
+++ AltenVehiclesMS/DBContexts/VehicleContext.cs
@@ -0,0 +1,51 @@
+﻿using AltenVehiclesMS.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AltenVehiclesMS.DBContexts
+{
+    public class VehicleContext : DbContext
+    {
+        public VehicleContext(DbContextOptions<VehicleContext> options) : base(options)
+        {
+        }
+
+        public DbSet<vehicle> Vehicles { get; set; }
+        public DbSet<VehiclePingHistory> VehiclePingHistory { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Customer>().HasData(
+                new Customer
+                {
+                    Id = 1,
+                    Name = "Kalles Grustransporter AB",
+                    Address = "Cementvägen 8, 111 11 Södertälje",
+                    //Vehicles = new List<Vehicle>()
+                },
+                new Customer
+                {
+                    Id = 2,
+                    Name = "Johans Bulk AB",
+                    Address = "Balkvägen 12, 222 22 Stockholm",
+                    // Vehicles = new List<Vehicle>()
+                },
+                new Customer
+                {
+                    Id = 3,
+                    Name = "Haralds Värdetransporter AB",
+                    Address = "Budgetvägen 1, 333 33 Uppsala",
+                    //Vehicles = new List<Vehicle>()
+                }
+                );
+
+
+
+        }
+
+
+    }
+}
