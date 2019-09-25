baef2a92d5c3e32e9185a854f0ed16dfe3c3928b
Horeya Mostafa <39738542+HoreyaMostafa@users.noreply.github.com>
2019-09-25 21:42:22 +0200

    Adding CustomersXUnit Project & Veichle project

---
+++ AltenCustomers.xUnitTests/CustomerRepositoryFake.cs
@@ -0,0 +1,51 @@
+﻿using AltenCustomersMS.Models;
+using AltenCustomersMS.Repository;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AltenCustomers.xUnitTests
+{
+    public class CustomerRepositoryFake: ICustomerRepository
+    {
+        private readonly IEnumerable<Customer> customers;
+
+        public CustomerRepositoryFake()
+        {
+            customers = new List<Customer>()
+            {
+                new Customer
+                {
+                    Id = 1,
+                    Name = "Kalles Grustransporter AB",
+                    Address = "Cementvägen 8, 111 11 Södertälje",
+                    //Vehicles = new List<Vehicle>()
+                },
+                new Customer
+                {
+                    Id = 2,
+                    Name = "Johans Bulk AB",
+                    Address = "Balkvägen 12, 222 22 Stockholm",
+                    // Vehicles = new List<Vehicle>()
+                },
+                new Customer
+                {
+                    Id = 3,
+                    Name = "Haralds Värdetransporter AB",
+                    Address = "Budgetvägen 1, 333 33 Uppsala",
+                    //Vehicles = new List<Vehicle>()
+                }
+            };
+        }
+
+        public IEnumerable<Customer> GetAllCustomers()
+        {
+            return customers;
+        }
+
+        public Customer GetCustomerByID(int CustomerId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ AltenCustomers.xUnitTests/CustomersControllerUTest.cs
@@ -0,0 +1,43 @@
+using AltenCustomersMS.Controllers;
+using AltenCustomersMS.Models;
+using AltenCustomersMS.Repository;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace AltenCustomers.xUnitTests
+{
+    public class CustomersControllerUTest
+    {
+        ICustomerRepository customerRepository;
+        CustomerController customerController;
+
+        public CustomersControllerUTest()
+        {
+            customerRepository = new CustomerRepositoryFake();
+            customerController = new CustomerController(customerRepository);
+        }
+
+        [Fact]
+        public void Getall_ReturnsOkResult()
+        {
+            // Act
+            var okResult = customerController.Get();
+
+            // Assert
+            Assert.IsType<OkObjectResult>(okResult);
+        }
+
+        [Fact]
+        public void Get_ReturnsAllItems()
+        {
+            // Act
+            var okResult = customerController.Get() as OkObjectResult;
+
+            // Assert
+            var items = Assert.IsType<List<Customer>>(okResult.Value);
+            Assert.Equal(3, items.Count);
+        }
+    }
+}
