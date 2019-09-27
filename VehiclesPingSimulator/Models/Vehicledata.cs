b9d75d44e80c59ee099abe548a5d4f70f18e316f
Horeya Mostafa <39738542+HoreyaMostafa@users.noreply.github.com>
2019-09-27 16:17:38 +0200

    PingSimultor

--- VehiclesPingSimulator/Models/Vehicledata.cs
+++ VehiclesPingSimulator/Models/Vehicledata.cs
@@ -4,12 +4,30 @@ using System.Text;
 
 namespace VehiclesPingSimulator.Models
 {
-    public class Vehicledata
+    public class Vehicledata : IComparable<Vehicledata>
     {
             public int ID { get; set; }
             public string VIN { get; set; }
             public string Regnr { get; set; }
             public int CustomerId { get; set; }
             public DateTime? LastSeen { get; set; }
+
+        public int CompareTo(Vehicledata other)
+        {
+            if (other == null)
+                return 1;
+            if (this.ID != other.ID)
+                return 1;
+            return 0;
+        }
+        public override bool Equals(object x)
+        {
+            return this.CompareTo(x as Vehicledata) == 0;
+        }
+
+        public override int GetHashCode()
+        {
+            return this.ID;
+        }
     }
 }
--- VehiclesPingSimulator/Program.cs
+++ VehiclesPingSimulator/Program.cs
@@ -1,19 +1,57 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using VehiclesPingSimulator.Models;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace VehiclesPingSimulator
 {
     class Program
     {
+        static HttpClient client = new HttpClient();
         static void Main(string[] args)
         {
-            
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+            IConfigurationRoot configuration = builder.Build();
+
+            RunAsync().GetAwaiter().GetResult();
+
         }
 
-
-        static IEnumerable<Vehicledata> GetAvailableVehicles()
+        static async Task RunAsync()
         {
+            // Get the product
+            var resullt = await GetAvailableVehiclesAsync();
+            foreach(Vehicledata vd in resullt)
+            {
+                Console.WriteLine($"ID: {vd.ID}\tPrice: " +
+              $"{vd.VIN}\VIN: {vd.Regnr}");
+            }
+
+        }
+        static async Task<IEnumerable<Vehicledata>> GetAvailableVehiclesAsync()
+        {
+            string vehicleAPIURL = "https://localhost:44314/api/vehicle";//ConfigurationManager.AppSettings["VehiclesEndPoint"];
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            HttpResponseMessage response = await client.GetAsync(vehicleAPIURL);
+            if (response.IsSuccessStatusCode)
+            {
+                var vehiclesdata = await response.Content.ReadAsAsync<IEnumerable<Vehicledata>>();
+                return vehiclesdata;
+            }
+            else
+            {
+                Console.WriteLine("{0} ({1})", (int)response.StatusCode, response.ReasonPhrase);
+            }
             return null;
         }
 
